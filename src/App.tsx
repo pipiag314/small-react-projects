0d95b2104c8fa24896184cb7cdcba99d9e854c0b
gela pipia <gelapipia10@gmail.com>
2024-02-08 22:05:53 +0400

    ADD: styled accordion component

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import Accordion from './components/Accordion/Accordion'
 
 function App() {
   return (
-    <main>
-      <h1 className='text-[red] text-8xl font-bold'>hello world</h1>
-      <Accordion />
+    <main className=''>
+      <section className='h-screen flex flex-col justify-start items-center mt-10'>
+        <h1 className='text-[red] text-8xl font-bold'>hello world</h1>
+        <Accordion />
+      </section>
     </main>
   )
 }
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -28,19 +28,19 @@ const Accordion = () => {
         <label htmlFor="checkbox">Enable Multy Selection:</label>
         <input type="checkbox" onChange={() => setMultySelection(!multySelection)} id="checkbox" className="w-5" />
       </div>
-      <div className="">
+      <div className="flex flex-col gap-4 items-center">
         {data && data.length > 0 ? (
           data.map((item) => (
             <div
-              className="cursor-pointer"
+              className="cursor-pointer bg-blue-500 min-h-[50px] w-[600px]"
               key={item.id}
               onClick={() => handleSelection(item.id)}>
-              <div className="flex gap-2 justify-center text-xl">
+              <div className="flex gap-2 justify-between p-2 items-center before:content-[''] text-xl">
                 <h2>{item.question}</h2>
                 <span>{active?.includes(item.id) ? "-" : "+"}</span>
               </div>
               {active?.includes(item.id) && (
-                <p className="text-lg text-[gray]">{item.answer}</p>
+                <p className="text-lg text-slate-300">{item.answer}</p>
               )}
             </div>
           ))
