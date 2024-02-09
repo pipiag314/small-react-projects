67215d806c0816f5082f50cf0a06ae2c83bd12cd
gela pipia <gelapipia10@gmail.com>
2024-02-09 14:24:10 +0400

    Random Color Generator component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Accordion from './components/Accordion/Accordion'
+import RandomColorGenerator from './components/RandomColorGenerator/RandomColorGenerator'
 
 
 function App() {
@@ -8,6 +9,10 @@ function App() {
       <section className='h-screen flex flex-col justify-start items-center mt-10'>
         <h1 className='text-[red] text-8xl font-bold'>hello world</h1>
         <Accordion />
+      </section>
+      <section className='h-screen flex flex-col justify-start items-center'>
+        <h1 className='text-[red] text-8xl font-bold'>Random Color Generator</h1>
+        <RandomColorGenerator />
       </section>
     </main>
   )
