ac5aaaa192e6bee45c84e6897ae457a7a1f14008
gela pipia <gelapipia10@gmail.com>
2024-02-09 15:10:48 +0400

    FEAT: copy functionality

--- src/components/RandomColorGenerator/RandomColorGenerator.tsx
+++ src/components/RandomColorGenerator/RandomColorGenerator.tsx
@@ -1,31 +1,66 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { randomBetween0_255 } from "./helper";
 
-const randomBetween0_255 = () => {
-    return Math.floor(Math.random() * 256);
-}
+const InputComponent = ({
+  red,
+  green,
+  blue,
+}: {
+  red: number;
+  green: number;
+  blue: number;
+}) => {
+  const inputRef = useRef<null | HTMLInputElement>(null);
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopy = () => {
+    if (!inputRef.current) return;
+    navigator.clipboard.writeText(inputRef.current.value);
+    setIsCopied(true);
+  };
+
+  return (
+    <div className="flex justify-center gap-5">
+      <input
+        ref={inputRef}
+        id="rgb-color"
+        disabled
+        value={`rgb(${red},${green},${blue})`}
+      />
+      <button onClick={handleCopy}>{isCopied ? "Copied" : "Copy"}</button>
+    </div>
+  );
+};
 
 const RandomColorGenerator = () => {
+  const [red, setRed] = useState(randomBetween0_255());
+  let green = randomBetween0_255();
+  let blue = randomBetween0_255();
 
-    const [red, setRed] = useState(randomBetween0_255());
-    let green = randomBetween0_255();
-    let blue = randomBetween0_255();
+  const handleClick = () => {
+    const newRed = randomBetween0_255();
+    setRed(newRed);
 
+    green = randomBetween0_255();
+    blue = randomBetween0_255();
+  };
 
-    const handleClick = () => {
-        const newRed = randomBetween0_255();
-        setRed(newRed);
+  return (
+    <div className="mt-10 flex flex-col gap-2 items-center">
+      <button
+        onClick={handleClick}
+        className="border-2 border-red-400 p-2 rounded-xl">
+        Generate Random Color
+      </button>
 
-        green = randomBetween0_255();
-        blue = randomBetween0_255();
-    }
+      {/* div for display color */}
+      <div
+        className={`w-[200px] h-[200px]`}
+        style={{ backgroundColor: `rgb(${red},${green},${blue})` }}></div>
 
-    return (
-        <div className="mt-10 flex flex-col gap-2">
-            <button onClick={handleClick} className="border-2 border-red-400 p-2 rounded-xl">Generate Random Color</button>
-            <div className={`w-[200px] h-[200px]`} style={{backgroundColor: `rgb(${red},${green},${blue})`}}></div>
-            <h1>{`rgb(${red},${green},${blue})`}</h1>
-        </div>
-    )
-}
+      <InputComponent red={red} green={green} blue={blue} />
+    </div>
+  );
+};
 
 export default RandomColorGenerator;
---
+++ src/components/RandomColorGenerator/helper.ts
@@ -0,0 +1,3 @@
+export const randomBetween0_255 = () => {
+    return Math.floor(Math.random() * 256);
+};
